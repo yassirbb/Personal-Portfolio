a8eca7b636693680e9e3cf7d50c934d82c537054
yassir <ybenboubker-ext@centreon.com>
2025-10-12 10:36:03 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Tailwind + Vite + React 💨
+      </h1>
+    </div>
+  );
+}
